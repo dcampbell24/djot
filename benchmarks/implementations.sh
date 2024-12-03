b05760914254e81a194b2e4533484891eaff13cd
David Campbell <dcampbell24@gmail.com>
2024-12-03 02:04:17 -0500

    Make the Markdown table more pretty.

--- benchmarks/implementations.sh
+++ benchmarks/implementations.sh
@@ -5,9 +5,9 @@ PANDOC_MANUAL_DJ=pandoc-manual.dj
 
 wget --no-clobber --output-document=$PANDOC_MANUAL_DJ $PANDOC_MANUAL
 
-hyperfine --warmup 20 --shell=none --export-markdown implementations.md \
-"$HOME/go/bin/godjot $PANDOC_MANUAL_DJ" \
-"$HOME/.cabal/bin/djoths $PANDOC_MANUAL_DJ" \
-"djot $PANDOC_MANUAL_DJ" \
-"$HOME/.luarocks/bin/djot $PANDOC_MANUAL_DJ" \
-"jotdown $PANDOC_MANUAL_DJ"
+hyperfine --warmup 20 --shell=none --export-markdown implementations.md --sort mean-time \
+--command-name Go "$HOME/go/bin/godjot $PANDOC_MANUAL_DJ" \
+--command-name Haskell "$HOME/.cabal/bin/djoths $PANDOC_MANUAL_DJ" \
+--command-name JavaScript "djot $PANDOC_MANUAL_DJ" \
+--command-name Lua "$HOME/.luarocks/bin/djot $PANDOC_MANUAL_DJ" \
+--command-name Rust "jotdown $PANDOC_MANUAL_DJ"
